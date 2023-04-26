cfdf41d06cf7f3f5c1b0b67409068569ca47dae1
Batya Zamansky <bzamansky@Batyas-MBP.wifi.gsu.edu>
2023-04-26 14:56:33 -0400

    starting transitions in cards

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
-var informationFlow = [];
+var informationFlow = ["#jobProcess","#resumeIntro","#resumeRules","#resumeExamples","#applicationsIntro","#applicationsFindJobs","#interviewsIntro","#interviewAdvice","#interviewingPriorities","#interviewsOtherTypes","#interviewsBehavioral"];
+var chosens = []
 
 $(document).ready(function() {
+  
+  $('#submitJobType').click(function(){
+    var chosen = $('input[type=radio]:checked').each(
+      function() {
+        chosens.push($(this).val())
+      }
+    )
 
-  $('#submit').click(function(){
-    var chosen_list = [];
-    var chosen = $('input[type=radio]:checked')
-      .each(function(){
-        chosen_list.push($(this).val());
-      });
-    var chosen_checks = $('input[type=checkbox]:checked')
-    .each(function(){
-      chosen_list.push($(this).val());
-    });
-
-    if (chosen_list.includes("idk")) {
-        if (chosen_list.includes("underOne")) {
-            informationFlow.push("#fullTimeJob")
-        } else {
-            informationFlow.push("#internship")
-        }
+    if (chosens.includes("fte")){
+      $("#fteBlurb").css('display','block');
+    } else {
+      $("#timeTilGrad").css('display','block');
     }
+  })
 
-    $("#question_form").css('display','none');
-    for (var i = 0; i < informationFlow.length; i++) {
-      var k = informationFlow[i];
-      $(k).css('display', 'block');
+  $('#timeTilGrad').click(function(){
+    var chosen = $('input[type=radio]:checked').each(
+      function() {
+        chosens.push($(this).val())
+      }
+    )
+
+    if (chosens.includes("underOne")){
+      $("#timeTilGradSummer").css('display','block');
+    } else {
+      $("#whyInternship").css('display','block');
     }
-
-  });
-
-  $("#reset").click(function(){
-    $("#question_form").css('display','block');
-    $('#answers').css('display','none');
-    $("#yougot").html('');
-    $("#desc").html('');
-    $(".check_img").css('background-image', "url('unchecked.png')");
-    $('input[type=radio]').prop('checked','false');
-    $('label').css('background-color', '#f2f2f2');
-    $('li').css('background-color', '#f2f2f2');
-
-  });
-
+  })
+  
+ 
 
 });
