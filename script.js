a4891cec62bd86e7956174a0534080566429624e
Batya Zamansky <bzamansky@Batyas-MBP.wifi.gsu.edu>
2023-04-27 15:54:04 -0400

    further progress on interactivity

--- script.js
+++ script.js
@@ -17,20 +17,81 @@ $(document).ready(function() {
     }
   })
 
-  $('#timeTilGrad').click(function(){
+  $('#submitTimeToGrad').click(function(){
     var chosen = $('input[type=radio]:checked').each(
       function() {
         chosens.push($(this).val())
       }
     )
-
     if (chosens.includes("underOne")){
       $("#timeTilGradSummer").css('display','block');
     } else {
       $("#whyInternship").css('display','block');
     }
   })
+
+  $('#submitSummerBtwn').click(function(){
+    var chosen = $('input[type=radio]:checked').each(
+      function() {
+        chosens.push($(this).val())
+      }
+    )
+    if (chosens.includes("noSummer")){
+      $("#noSummerBetweenFTE").css('display','block');
+    } else {
+      $("#whyInternship").css('display','block');
+    }
+  })
   
+  $("#whyInternship i").click(function() {
+    $("#courseworkTaken").css('display','block');
+  });
  
+  $("#submitCourseworkTaken").click(function() {
+    var chosen = $('input[type=checkbox]:checked').each(
+      function() {
+        chosens.push($(this).val())
+      }
+    )
+
+    // Has only taken intro, or no CS courses, and >3 years to grad
+    if ((introOnly() || noneOnly()) && chosens.includes('overthree')) {
+      $("#freshmanIntern").css('display', 'block');
+    // Has only taken intro, or no CS courses, and <3 years to grad
+    } else if (introOnly() || noneOnly()){
+      $("#introOnlyIntern").css('display', 'block');
+    // Has taken more than just intro courses
+    } else if (introPlus()) {
+      $("#internBlurb").css('display', 'block');
+    }
+
+  });
 
 });
+
+function introOnly() {
+  var hasIntro = chosens.includes('intro');
+  var hasDS = chosens.includes('datasctructures');
+  var hasAlgo = chosens.includes('algo');
+  var hasNone = chosens.includes('none');
+
+  return hasIntro && !hasDS && !hasAlgo && !hasNone;
+}
+
+function noneOnly() {
+  var hasIntro = chosens.includes('intro');
+  var hasDS = chosens.includes('datasctructures');
+  var hasAlgo = chosens.includes('algo');
+  var hasNone = chosens.includes('none');
+
+  return !hasIntro && !hasDS && !hasAlgo && hasNone;
+}
+
+function introPlus() {
+  var hasIntro = chosens.includes('intro');
+  var hasDS = chosens.includes('datasctructures');
+  var hasAlgo = chosens.includes('algo');
+  var hasNone = chosens.includes('none');
+
+  return hasIntro && (hasDS || hasAlgo);
+}
